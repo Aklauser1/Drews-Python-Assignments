cc21e3ceb5fd6b51a3b5888fd46404bdc7adf601
Aklauser1 <31425004+Aklauser1@users.noreply.github.com>
2018-09-11 19:56:36 -0500

    Update bitcoin_converter.py

--- bitcoin_converter.py
+++ bitcoin_converter.py
@@ -1,8 +1,10 @@
 #bitcoin to dollar converter
+# print statement notifying the user of the time and date of the conversion standard we are using, then asks for input 
 print("As of 9/11/2018 at 7:00pm, bitcoin is currently trading at $6,289.85 per bitcoin.")
 amountInBitcoin = float(input("Enter the bitcoin amount:"))
 
 # process for Dollars to Bitcoin
 amountInDollars = (amountInBitcoin * 6289.85)
 
+# print the converted amount in U.S. Dollars to the user 
 print("That is worth " + format(amountInDollars, ".2f") + " U.S. Dollars")
